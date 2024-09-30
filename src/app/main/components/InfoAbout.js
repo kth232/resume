ccd8a6345873869face2095af8b05d9ddc09a0aa
kth232 <kth7537@naver.com>
2024-09-30 13:02:49 +0900

    포트폴리오 기초 구성

---
+++ src/app/main/components/InfoAbout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Section } from 'react-fullpage';
+
+const InfoAbout = () => {
+  return (
+    <Section>
+      <h1>INFO & ABOUT</h1>
+    </Section>
+  );
+};
+
+export default React.memo(InfoAbout);
